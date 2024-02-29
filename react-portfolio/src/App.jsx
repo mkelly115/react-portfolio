22cb7e394b9fb91ebe2afc789b2a520ffe45a3cc
michael <mikek222@gmail.com>
2024-02-29 19:07:24 +0300

    switched to bootstrap as CSS base

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+// {import { useState } from 'react'}
+// {import reactLogo from './assets/react.svg'}
 import './App.css'
+import Nav from './components/Navigation/index'
 
 function App() {
 
   return (
       <div>
-       
+       <Nav />
     </div>
   )
 }
---
+++ react-portfolio/src/components/Navigation/index.jsx
@@ -0,0 +1,26 @@
+export default function Nav() {
+    
+
+    return (
+      <nav className='nav justify-content-end'>
+        <section className=''>
+          Michael Kelly
+        </section>
+        <ul>
+          <li className='nav-item'>
+            <a className='nav-link' href="#">About</a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href="#">Portfolio</a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href="#">Resume</a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+  
